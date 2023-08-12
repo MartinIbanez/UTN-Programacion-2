6bd049847a894e404b62ecc4cf27d60940a79edb
Martin Ibañez <107942533+MartinIbanez@users.noreply.github.com>
2023-08-12 01:44:32 -0300

    funciones.h

--- EjercicioClase1/funciones.h
+++ EjercicioClase1/funciones.h
@@ -3,6 +3,11 @@
 
 int menu();
 void showItem(const char* text, int posx, int posy, bool select);
-
+void cargarMaterias(int numMateria[],int const tam,char materia[][20]);
+void mostrarMaterias(int numMateria[],int const tam,char materia[][20]);
+void cargarCadena(char materia[][20],int const tam, int pos);
+void cargarIngresos(int diaMarzo[31],float horasMaterias[]);
+void buscarAccesos(float horasMaterias[],int tam,char materia[][20]);
+void mayorAcceso(float horasMaterias[],int tam);
 
 #endif // FUNCIONES_H_INCLUDED
