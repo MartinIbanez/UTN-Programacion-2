4dfd80b0da7eca425159e306d1f6d117ae99be86
Martin Ibañez <107942533+MartinIbanez@users.noreply.github.com>
2023-09-21 16:43:48 -0300

    Empleado.h

---
+++ TP2-Clases/Empleado.h
@@ -0,0 +1,24 @@
+#ifndef EMPLEADO_H
+#define EMPLEADO_H
+#include "Fecha.h"
+
+
+class Empleado{
+private:
+    int _dni;
+    std::string _nombre,_apellido,_domicilio,_email;
+    Fecha f;
+
+
+public:
+
+
+
+
+
+
+
+
+};
+
+#endif // EMPLEADO_H
