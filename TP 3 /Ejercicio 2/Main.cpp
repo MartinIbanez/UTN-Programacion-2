010a583a3d0aecfeeda11f6811fb6643e025d1a2
Martin Ibañez <107942533+MartinIbanez@users.noreply.github.com>
2023-09-25 14:23:51 -0300

    Main.cpp

--- TP 3 /Ejercicio 2/Main.cpp
+++ TP 3 /Ejercicio 2/Main.cpp
@@ -1,1 +1,31 @@
+#include <iostream>
+#include<cstdlib> ///PARA LIMPIAR LA PANTALLA
+#include <stdlib.h> ///PARA LA PAUSA.
+#include <ctime>     ///LIBRERIA PARA NUMERO ALEATORIO.
+#include <string.h> ///LIBRERIA PARA CADENA DE CARACTERES.
+#include <cstring>  ///COPIA UNA PALABRA EN OTRA.
+#include <conio.h> ///PARA USAR GETCH.
+#include <string> ///ELEMENTOS TIPO CHAR
+#include<locale.h> ///PARA CARACTERES ESPECIALES.
+#include<windows.h> ///PARA EL LOGO DE CARD JITSU Y COLOR.
+#include <clocale> ///PARA CARACTERES ESPECIALES.
+using namespace std;
+#include "Fecha.h"
+#include "Investigador.h"
+
+int main(){
+    Investigador Martin;
+
+    Martin.cargarInvestigador();
+
+    cout<<endl;
+
+    Martin.mostrarInvestigador();
+
+
+
+
+
+    return 0;
+}
 
