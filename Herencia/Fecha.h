8f46bc6f0fb9c00781d40d408fd323e5a31ceb5d
Martin Ibañez <107942533+MartinIbanez@users.noreply.github.com>
2023-10-31 00:07:16 -0300

    Create Fecha.h

---
+++ Herencia/Fecha.h
@@ -0,0 +1,33 @@
+#ifndef FECHA_H_INCLUDED
+#define FECHA_H_INCLUDED
+using namespace std;
+
+class Fecha{
+    private:
+        int dia, mes, anio;
+    public:
+        void Cargar(){
+            cout<<"DIA: ";
+            cin>>dia;
+            cout<<"MES: ";
+            cin>>mes;
+            cout<<"ANIO: ";
+            cin>>anio;
+        }
+        void Mostrar(){
+            cout<<"DIA: "<<dia<<endl;
+            cout<<"MES: "<<mes<<endl;
+            cout<<"ANIO: "<<anio<<endl;
+        }
+        void mostrarEnLinea(){
+            cout<<dia<<"/"<<mes<<"/"<<anio<<endl;
+        }
+
+        void setDia(int d){
+            if(d>=1 && d<=31) dia=d;
+            else dia=-1;
+        }
+        int getDia(){return dia;}
+};
+
+#endif // FECHA_H_INCLUDED
