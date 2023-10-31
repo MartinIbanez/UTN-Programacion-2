c3250cbbd7024217a0956af02d85bb9c4cb93855
Martin Ibañez <107942533+MartinIbanez@users.noreply.github.com>
2023-10-31 00:07:32 -0300

    Create Main.cpp

---
+++ Herencia/Main.cpp
@@ -0,0 +1,24 @@
+# include<iostream>
+# include<cstdlib>
+# include<cstring>
+
+using namespace std;
+
+#include "Fecha.h"
+#include "Alumno.h"
+
+
+int main(){
+    Alumno aux;
+   // aux.Cargar();
+	cout<<endl<<endl;
+	//aux.Mostrar();
+
+
+
+
+
+	system("pause");
+	return 0;
+}
+
